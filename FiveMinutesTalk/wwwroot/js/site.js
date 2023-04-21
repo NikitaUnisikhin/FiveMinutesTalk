83206f17d53f37a1e4d5194612f7fcf667c9c3f6
Alexander Skomorokhov <skomor.ae@gmail.com>
2023-04-21 16:13:25 +0500

    Добавил возможность ввода ответов к типу вопроса с одним вариантом ответа.

--- FiveMinutesTalk/wwwroot/js/site.js
+++ FiveMinutesTalk/wwwroot/js/site.js
@@ -4,7 +4,7 @@
 // Write your JavaScript code.
 
 // Перенес сюда подключение кнопки добавления к функции, т.к. писать onclick в html плохая практика
-let addQuestion = document.querySelector('#add');
+let addQuestion = document.querySelector('#addQuestion');
 addQuestion.addEventListener('click', newField);
 
 // Я предлагаю сделать глобальный id для вопросов, чтобы после удаления у нас не было совпадений id вопросов
@@ -100,9 +100,42 @@ function changeComponents(questionType){
     } else if (selectedValue === "Код"){
         newDiv.innerText = "Код";
     } else if (selectedValue === "Один из списка"){
-        newDiv.innerText = "Один из списка";
+        // здесь создаем кнопку, которая добавляет новый label с ratio кнопкой и вводом
+        let addRadio = document.createElement("input");
+        addRadio.setAttribute("value", "Добавить вариант ответа");
+        addRadio.setAttribute("type", "button");
+        addRadio.setAttribute("id", "addRadio[" + questType.id + "]");
+        addRadio.addEventListener("click", function(e) {
+            addNewRatio(e)
+        });
+        newDiv.appendChild(addRadio);
     } else if (selectedValue === "Несколько из списка"){
         newDiv.innerText = "Несколько из списка";
     }
     cont.after(newDiv);
+}
+
+// функция по добавлению новых label с ratio и вводом
+function addNewRatio(e){
+    // получаем из события нажатия, на каком объекте это произошло
+    let addRad = e.currentTarget;
+    
+    let label = document.createElement("label");
+    label.setAttribute("id", addRad.id);
+    label.setAttribute("class", "ratio-label");
+    
+    let ratio = document.createElement("input");
+    ratio.setAttribute("type", "radio");
+    ratio.setAttribute("name", 'Ratio[' + addRad.id + ']')
+    ratio.setAttribute("id", addRad.id);
+    
+    let ratioText = document.createElement("input");
+    ratioText.setAttribute("placeholder", "Напишите вариант ответа");
+    ratioText.setAttribute("type", "text");
+    ratioText.setAttribute("id", addRad.id);
+    
+    label.appendChild(ratio);
+    label.appendChild(ratioText);
+    
+    addRad.before(label);
 }
