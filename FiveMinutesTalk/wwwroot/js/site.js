cb1ebc4f24017edc2e61c92a53f1f95ddd4e3f60
Alexander Skomorokhov <skomor.ae@gmail.com>
2023-05-04 00:28:09 +0500

    JS to HTML with clone div

--- FiveMinutesTalk/wwwroot/js/site.js
+++ FiveMinutesTalk/wwwroot/js/site.js
@@ -3,106 +3,42 @@
 
 // Write your JavaScript code.
 
-// Перенес сюда подключение кнопки добавления к функции, т.к. писать onclick в html плохая практика
-let addQuestion = document.querySelector('#addQuestion');
-addQuestion.addEventListener('click', newField);
+// Подключение кнопки добавления к функции
+document.querySelector('#addQuestion').addEventListener('click', newField);
+
+// Подключение изначального select к функции
+document.querySelector('select').addEventListener('change', function(questionType) {
+    changeComponents(questionType)
+});
+
+// Подключение кнопки закрытия к функции
+document.querySelector(".close-button").addEventListener('click', removeParent);
+
+let baseForm = document.getElementById("Form1");
+
+let section = document.getElementById("questions");
+
+const copied = baseForm.cloneNode(true);
 
 // Я предлагаю сделать глобальный id для вопросов, чтобы после удаления у нас не было совпадений id вопросов
 let questionId = 1;
 
 function newField() {
-    // определяем контейнер для хранения полей с вопросами
-    let container = document.getElementById("questions");
-    // получаем текущее количество input (полей для вопросов)
-    let fieldCount = container.getElementsByClassName("form-group").length;
-    // увеличиваем No для нового поля
-    let nextFieldNo = fieldCount + 1;
-
-    // здесь добавляем элемент, который будет хранить input
-    let div = document.createElement("div");
-    div.setAttribute("class", "form-group");
-    
-    // создаем новое поле с новым id, name ДОЛЖЕН СОВПАДАТЬ С ИМЕНЕМ ПОЛЯ В МОДЕЛИ!!!
-    let field = document.createElement("input");
-    field.setAttribute("class", "form-control");
-    field.setAttribute("id", "Questions[" + questionId + "]");
-    field.setAttribute("name", "Questions");
-    field.setAttribute("type", "text");
-    field.setAttribute("asp-for", "Text");
-    field.setAttribute("placeholder", "Введите текст вопроса");
-
-    // а здесь я своими кривыми ручками создаю номера вопросов
-    const questionNumber = document.createTextNode(String(nextFieldNo));
-    div.appendChild(questionNumber);
-    
-    // Здесь создаем выбор типа вопроса
-    let lbl = document.createElement("label");
-    let spn = document.createElement("span");
-    spn.textContent = "Тип вопроса: ";
-    let questionType = document.createElement("select");
-    questionType.id = String(questionId);
-    
-    // создаем options для select
-    // изменил обратно, чтобы была возможность добавить аттрибут для типа вопросов
-    let opt1 = document.createElement("option");
-    opt1.setAttribute("value", "0");
-    let nod1 = document.createTextNode("Текст");
-    opt1.appendChild(nod1);
-
-    let opt2 = document.createElement("option");
-    opt2.setAttribute("value", "1");
-    let nod2 = document.createTextNode("Код");
-    opt2.appendChild(nod2);
-
-    let opt3 = document.createElement("option");
-    opt3.setAttribute("value", "2");
-    let nod3 = document.createTextNode("Один из списка");
-    opt3.appendChild(nod3);
-
-    let opt4 = document.createElement("option");
-    opt4.setAttribute("value", "3");
-    let nod4 = document.createTextNode("Несколько из списка");
-    opt4.appendChild(nod4);
-    
-    // добавляем options
-    questionType.appendChild(opt1);
-    questionType.appendChild(opt2);
-    questionType.appendChild(opt3);
-    questionType.appendChild(opt4);
-    
-    // При изменении типа будем менять компонеты вопроса
-    questionType.addEventListener('change', function(questionType) {
+    questionId++;
+    let newCopied = copied.cloneNode(true);
+    newCopied.id = "Form" + questionId;
+    let select = newCopied.querySelector("select");
+    select.id = String(questionId);
+    select.addEventListener('change', function(questionType) {
         changeComponents(questionType)
     });
-    
-    lbl.appendChild(spn);
-    lbl.appendChild(questionType);
-    
-    // добавляем всё это в div
-    div.appendChild(lbl);
-
-    // Здесь создаю кнопку для удаления вопроса
-    let close = document.createElement("button");
-    close.setAttribute("class", "close-button");
-    close.addEventListener('click', removeParnet);
-    close.textContent = "X";
-    div.appendChild(close);
-    
-    // добавляем поле в <div class="form-group"></div>
-    div.appendChild(field);
-    
-    // Поле для ответа(здесь будут варианты выбора ответа и т.д.)
-    let div2 = document.createElement("div");
-    div.appendChild(div2);
-    
-    // добавляем <div class="form-group"><input ... /></div> в главный контейнер
-    container.appendChild(div);
-    
-    // Если удачно всё выполнилось до этого и создался новый вопрос, инкрементируем
-    questionId++;
+    newCopied.querySelector("#Question1").id = "Question" + questionId;
+    newCopied.querySelector("#Answer1").id = "Answer" + questionId;
+    newCopied.querySelector(".close-button").addEventListener('click', removeParent);
+    section.appendChild(newCopied);
 }
 
-function removeParnet(){
+function removeParent(){
     let revDiv = this.parentElement;
     revDiv.remove();
 }
@@ -112,26 +48,28 @@ function changeComponents(questionType){
     // Получаем элемент на котором произошло событие
     let questType = questionType.currentTarget;
     let selectedValue = questType.value;
-    // здесь, зная id, мы получаем контейнер с вопросом(в дальнейшем я изменю этот костыль)
-    let cont = document.getElementById("Questions[" + questType.id + "]");
-    let nextDiv = cont.nextSibling;
-    nextDiv.remove();
+    // здесь, зная id, мы получаем контейнер с вопросом
+    console.log(questType.id)
+    let cont = document.getElementById("Answer" + questType.id);
+    cont.remove();
     let newDiv = document.createElement("div");
+    newDiv.setAttribute("id", "Answer" + questType.id);
     
     if (selectedValue === "0"){
+        console.log(1);
         newDiv.innerText = "Текст";
         
     } else if (selectedValue === "1"){
         newDiv.innerText = "Код";
         
     } else if (selectedValue === "2"){
-        // здесь создаем кнопку, которая добавляет новый label с ratio кнопкой и вводом
+        // здесь создаем кнопку, которая добавляет новый label с radio кнопкой и вводом
         let addRadio = document.createElement("input");
         addRadio.setAttribute("value", "Добавить вариант ответа");
         addRadio.setAttribute("type", "button");
-        addRadio.setAttribute("id", "addRadio[" + questType.id + "]");
+        addRadio.setAttribute("id", "AddRadio" + questType.id);
         addRadio.addEventListener("click", function(e) {
-            addNewRatio(e)
+            addNewRadio(e)
         });
         newDiv.appendChild(addRadio);
         
@@ -139,36 +77,37 @@ function changeComponents(questionType){
         let addCheckbox = document.createElement("input");
         addCheckbox.setAttribute("value", "Добавить вариант ответа");
         addCheckbox.setAttribute("type", "button");
-        addCheckbox.setAttribute("id", "addСheckbox[" + questType.id + "]");
+        addCheckbox.setAttribute("id", "AddСheckbox" + questType.id);
         addCheckbox.addEventListener("click", function(e) {
             addNewCheckbox(e)
         });
         newDiv.appendChild(addCheckbox);
     }
-    cont.after(newDiv);
+    let form = document.getElementById("Form" + questType.id);
+    form.appendChild(newDiv);
 }
 
 // функция по добавлению новых label с ratio и вводом
-function addNewRatio(e){
+function addNewRadio(e){
     // получаем из события нажатия, на каком объекте это произошло
     let addRad = e.currentTarget;
     
     let label = document.createElement("label");
     label.setAttribute("id", addRad.id);
-    label.setAttribute("class", "ratio-label");
+    label.setAttribute("class", "radio-label");
     
-    let ratio = document.createElement("input");
-    ratio.setAttribute("type", "radio");
-    ratio.setAttribute("name", 'Ratio[' + addRad.id + ']')
-    ratio.setAttribute("id", addRad.id);
+    let radio = document.createElement("input");
+    radio.setAttribute("type", "radio");
+    radio.setAttribute("name", addRad.id)
+    radio.setAttribute("id", addRad.id);
     
-    let ratioText = document.createElement("input");
-    ratioText.setAttribute("placeholder", "Напишите вариант ответа");
-    ratioText.setAttribute("type", "text");
-    ratioText.setAttribute("id", addRad.id);
+    let radioText = document.createElement("input");
+    radioText.setAttribute("placeholder", "Напишите вариант ответа");
+    radioText.setAttribute("type", "text");
+    radioText.setAttribute("id", addRad.id);
     
-    label.appendChild(ratio);
-    label.appendChild(ratioText);
+    label.appendChild(radio);
+    label.appendChild(radioText);
     
     addRad.before(label);
 }
@@ -183,7 +122,7 @@ function addNewCheckbox(e){
 
     let checkbox = document.createElement("input");
     checkbox.setAttribute("type", "checkbox");
-    checkbox.setAttribute("name", 'Checkbox[' + addCheck.id + ']')
+    checkbox.setAttribute("name", addCheck.id)
     checkbox.setAttribute("id", addCheck.id);
 
     let checkboxText = document.createElement("input");
