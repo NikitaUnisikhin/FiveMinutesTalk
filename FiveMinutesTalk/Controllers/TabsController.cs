e244e46ed82966e2e64d5da524c0be43db7d2d53
NikitaUnisikhin <nik.unisikhin.05@gmail.com>
2023-06-23 14:22:44 +0500

    fix

--- FiveMinutesTalk/Controllers/TabsController.cs
+++ FiveMinutesTalk/Controllers/TabsController.cs
@@ -1,24 +1,16 @@
-using FiveMinutesTalk.Domain;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FiveMinutesTalk.Controllers;
 
 public class TabsController : Controller
 {
-    private readonly DataManager _dataManager;
-
-    public TabsController(DataManager dataManager)
-    {
-        _dataManager = dataManager;
-    }
-    
     [HttpPost]
     public IActionResult Index(IFormCollection form, Guid quizId)
     {
         var statisticsType = ((FormCollection)form).Keys.FirstOrDefault();
         if (statisticsType == "1")
             return RedirectToAction("Quizzes", "Account", new { token = quizId });
-        else if (statisticsType == "2")
+        if (statisticsType == "2")
             return RedirectToAction("Index", "GeneralStatistics", new { token = quizId });
         return Empty;
     }
